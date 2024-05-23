d28c5ba58e3d954b6453298e23c98fbf869b22bf
jan-ole.schuemann@alstomgroup.com <jan-ole.schuemann@alstomgroup.com>
2024-05-23 11:59:18 +0700

    add demo code

---
+++ MudBlazorBug/MudBlazorBug/MudBlazorBug/Components/Pages/ColumnDesciption.cs
@@ -0,0 +1,34 @@
+﻿namespace MudDataGridBug
+{
+   
+
+    public class ColumnDesciption
+    {
+        public string name;
+        public Type? dataType;
+        public bool hasData = false;
+
+        //public static ColumnType GetTypeFromString(string typeStr)
+        //{
+        //    switch(typeStr.ToLower()) 
+        //    {
+        //        case "bigint":
+        //        case "smallint":
+        //        case "int":
+        //            return ColumnType.Integer;
+        //        case "float":
+        //        case "real":
+        //        case "double precision":
+        //            return ColumnType.Real;
+        //        case "varchar":
+        //        case "text":
+        //            return ColumnType.Text;
+        //        case "datetime":
+        //            return ColumnType.DateTime;
+        //        default:
+        //            return ColumnType.Unknown;  
+        //    }
+        //}
+    }
+
+}
---
+++ MudBlazorBug/MudBlazorBug/MudBlazorBug/Components/Pages/LogRowData.cs
@@ -0,0 +1,31 @@
+﻿namespace MudDataGridBug
+{
+    public class LogRowData
+    {
+        private List<object?> data = new List<object?>();
+
+        public LogRowData(List<object?> data) 
+        {
+            this.data = data;
+        }
+
+        public string GetString(int i)
+        {
+            string r = string.Empty;            
+            try
+            {
+                r = Convert.ToString(data[i]);
+            }
+            catch 
+            { 
+            }
+
+            return r;
+        }
+
+        public int GetRowIndex()
+        {
+            return Convert.ToInt32(data[0]);
+        }
+    }
+}
